9f2f23c8294949dd7a6bd043e229e87328d522f1
Sourav Kumar Jana <souravkumarjana111@gmail.com>
2026-01-07 12:48:46 +0530

    add .env file

--- lib/Widgets/get_weather_response.dart
+++ lib/Widgets/get_weather_response.dart
@@ -1,13 +1,15 @@
-import 'package:dio/dio.dart'; 
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; 
 
 class WeatherResponse {
   final dio = Dio();
-  final String apiKey = "ab0555c6e405b5dab41d7e306b5f0f64"; 
+  final String apiKey = dotenv.env['WEATHER_API_KEY'] ?? "";
+  final String base_url = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future<Map<String, dynamic>?> getWeather(double lat, double lon) async {
     try {
       final response = await dio.get(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
+        '$base_url?lat=$lat&lon=$lon&units=metric&appid=$apiKey',
       );
       
       if (response.statusCode == 200) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'widgets/app.dart';
-void main(){
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async{
+  await dotenv.load(fileName: '.env');
   runApp(const MyHome());
 }
 
